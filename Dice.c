f15c9180445b2f9e8efa452ff26713d89e0dbf51
五十嵐善史 <IgarashiYoshishi@igarashiyoshishi-no-MacBook-Pro.local>
2018-07-08 16:35:43 +0900

    initial revision

---
+++ Dice.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void){
+	
+  int n1,n2,sum;
+
+  srand((unsigned)time(NULL));
+  
+n1 = rand()%6+1;
+n2 = rand()%6+1;
+
+	sum=n1+n2;
+
+	printf("Rolling the dice...\n");
+	printf("Die 1: %d\nDie 2: %d\n",n1,n2);
+
+	printf("Total value: %d\n",sum);
+
+
+
+	return 0;
+}
